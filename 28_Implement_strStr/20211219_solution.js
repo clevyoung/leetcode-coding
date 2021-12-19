496aae185cc7b7c6d0cffbb3fb31792beaac02c5
clevyoung <clevyoung@gmail.com>
2021-12-19 16:43:05 +0900

    20211219 leetcode easy Implement strStr() 유튜브 솔루션

---
+++ 28_Implement_strStr/20211219_solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+const strStr = function (haystack, needle) {
+  if (!needle) {
+    return 0;
+  }
+
+  while (
+    haystack.substring(i, needle.length + i) !== needle &&
+    i <= haystack.length
+  ) {
+    i++;
+  }
+
+  return i > haystack.length ? -1 : i;
+};
+
+console.log(strStr('hello', 'll'));
+console.log(strStr('', ''));
+console.log(strStr('aaa', 'ba'));
