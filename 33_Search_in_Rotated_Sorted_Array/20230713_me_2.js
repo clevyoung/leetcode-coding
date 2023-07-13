4a0cd86f31741d0d7711adce0f975618fda191f4
clevyoung <clevyoung@gmail.com>
2023-07-13 10:36:43 +0900

    [medium] Search in Rotated Sorted Array

---
+++ 33_Search_in_Rotated_Sorted_Array/20230713_me_2.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.lengt - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    // left half
+    if (nums[left] <= nums[mid]) {
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      // right half
+      if (target <= nums[right] && target > nums[mid]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
